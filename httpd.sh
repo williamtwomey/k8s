1ba2ece89d899c10141ed6becb0cb4e1de8dfd19
will7925 <will.twomey@RACKSPACE.COM>
2018-10-26 16:10:58 -0500

    httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -2,5 +2,5 @@
 sed -i 's/DBUSER/'$WORDPRESS_DB_USER'/' /var/www/html/kube.bbis.us/wp-config.php
 sed -i 's/DBNAME/'$WORDPRESS_DB_NAME'/' /var/www/html/kube.bbis.us/wp-config.php
 sed -i 's/DBHOST/'$WORDPRESS_DB_HOST'/' /var/www/html/kube.bbis.us/wp-config.php
-sed -i 's/DBPASS/'$WORDPRESS_DB_PASS'/' /var/www/html/kube.bbis.us/wp-config.php
+sed -i 's/DBPASS/'$WORDPRESS_DB_PASSWORD'/' /var/www/html/kube.bbis.us/wp-config.php
 echo "Git pull worked `date`" > /tmp/hello

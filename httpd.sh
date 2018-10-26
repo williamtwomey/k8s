5894067b54f8bf06ce4ad2d244314ec21153bc7d
will7925 <will.twomey@RACKSPACE.COM>
2018-10-26 15:48:09 -0500

    httpd.sh

---
+++ httpd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sed 's/DBUSER/'$WORDPRESS_DB_USER'/' /var/www/html/vhost/kube.bbis.us/wp-config.php
+sed 's/DBNAME/'$WORDPRESS_DB_NAME'/' /var/www/html/vhost/kube.bbis.us/wp-config.php
+sed 's/DBHOST/'$WORDPRESS_DB_HOST'/' /var/www/html/vhost/kube.bbis.us/wp-config.php
+sed 's/DBPASS/'$WORDPRESS_DB_PASS'/' /var/www/html/vhost/kube.bbis.us/wp-config.php
+

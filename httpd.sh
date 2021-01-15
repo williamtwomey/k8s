72712c64dbc21a9eb22256fd33963d646e6e34b7
williamtwomey <william.twomey@gmail.com>
2021-01-14 20:29:28 -0500

    k8s

--- httpd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed -i 's/DBUSER/'$WORDPRESS_DB_USER'/' /var/www/html/kube.bbis.us/wp-config.php
-sed -i 's/DBNAME/'$WORDPRESS_DB_NAME'/' /var/www/html/kube.bbis.us/wp-config.php
-sed -i 's/DBHOST/'$WORDPRESS_DB_HOST'/' /var/www/html/kube.bbis.us/wp-config.php
-sed -i 's/DBPASS/'$WORDPRESS_DB_PASSWORD'/' /var/www/html/kube.bbis.us/wp-config.php
-echo "Git pull worked `date`" > /tmp/hello
